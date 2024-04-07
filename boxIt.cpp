1367e4939cdf3dff0630aa1875fda687ef12db01
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-08 01:04:01 +0600

    slove these solution

--- boxIt.cpp
+++ boxIt.cpp
@@ -2,7 +2,54 @@
 
 using namespace std;
 //Implement the class Box 
+class Box{
+    private:
+       int l,b,h;
+    public:
+        Box(){
+            l=0;
+            b=0;
+            h=0;
+        }
+        Box(int length,int breadth,int height){
+            l=length;
+            b=breadth;
+            h=height;
+        }
+        Box(const Box& B){
+            l=B.l;
+            b=B.b;
+            h=B.h;
+        }
+        
+        int getLength(){
+            return l;
+        }
+        int getBreadth(){
+            return b;
+        }
+        int getHeight(){
+            return h;
+        }
+        long long CalculateVolume(){
+            return (long long)l*b*h;
+        }
+        
+        friend bool operator < ( Box&A,Box& B){
+            if( (A.l < B.l) || ((A.b < B.b) && (A.l == B.l)) || ((A.h < B.h) && (A.b == B.b) && (A.l == B.l)) ){
+                return true;
+            }
+            else{
+                return false;
+            }
+        };
 
+    // Overload the << operator
+        friend ostream& operator<<(ostream& out, const Box& B) {
+            out << B.l << " " << B.b << " " << B.h;
+            return out;
+        }
+}; 
 //l,b,h are integers representing the dimensions of the box
 
 // The class should have the following functions : 
